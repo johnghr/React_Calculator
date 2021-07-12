5a3fb332f7be0860bad0b9caf0a3f442a70322fa
John Ross <johnyboy11@hotmail.co.uk>
2021-07-12 13:59:46 +0100

    added tests

--- cypress/integration/App.spec.js
+++ cypress/integration/App.spec.js
@@ -27,4 +27,22 @@ it('should be able to chain multiple operations', () => {
     cy.get('#operator_equals').click();
 })
 
+it('should be able to handle negative numbers', () => {
+  cy.get('#number1').click();
+  cy.get('#operator_subtract').click();
+  cy.get('#number2').click();
+  cy.get('#operator_equals').click();
+  cy.get('.display').should('contain', '-1')
+
+})
+
+it('should return infinity when dividing with zero', () => {
+  cy.get('#number1').click();
+  cy.get('#operator_divide').click();
+  cy.get('#number0').click();
+  cy.get('#operator_equals').click();
+  cy.get('.display').should('contain', 'Infinity')
+
+})
+
 })
